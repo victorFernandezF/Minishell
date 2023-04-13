f8bb4b2bda894fb27f351187be1d8d0ac374fa85
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-04-13 12:26:44 +0200

    Refactor: organize headers file

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:50:39 by victofer          #+#    #+#             */
-/*   Updated: 2023/04/12 18:20:46 by victofer         ###   ########.fr       */
+/*   Updated: 2023/04/13 12:26:06 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,34 @@ typedef struct s_cmd
 	t_cmd	*next;
 }			t_cmd;
 
+//	I N I T
+
+//	P A R S E R
+
 t_cmd	*start_parser(t_cmd *cmd, char *str);
-int		get_nb_cmd(char *str, int cmd);
 char	*get_part_from_str(char *str, int part);
+
+//	G E T   E A C H   P A R T   O F   C M D 
+
+int		get_nb_cmd(char *str, int cmd);
+char	*get_cmd(char *str);
+char	*get_flags(char *str);
+
+//	U T I L I T I E S
+
+int		skip_whitespaces(char *str, int i);
+
+//	C H E C K S
+
+int		are_there_flags(char *str);
+int		is_token(char c);
+
+//	F R E E   S T U F F 
 
 void	free_struct(t_cmd *cmd);
 void	free_array(char **array);
 
+//	L E A K S   T E M P 
 
 void	leaks(void);
 
