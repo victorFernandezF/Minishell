0a26e824c48d8e668fd532856af8825e3ff1eada
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-04-17 14:47:09 +0200

    Test: comment part to avoid error

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:50:03 by victofer          #+#    #+#             */
-/*   Updated: 2023/04/17 14:39:28 by victofer         ###   ########.fr       */
+/*   Updated: 2023/04/17 14:41:19 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_cmd	*fill_struct(t_cmd *cmd, char **command, int nb_cmd)
 	cmd->cmd = get_part_from_str(command[0], 1);
 	if (are_there_char(command[0], '-'))
 		cmd->flags = get_part_from_str(command[0], 2);
-	cmd->params = get_part_from_str(command[0], 3);
+	//cmd->params = get_part_from_str(command[0], 3);
 	cmd->output = get_output(command[0], cmd);
 	return (cmd);
 }
