1842f0db57c074a71a2ec13405c3ae883f3ef647
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-04-18 18:19:00 +0200

    Feat: add a function that prints the content of struct. just for testing

--- src/leaks.c
+++ src/leaks.c
@@ -6,11 +6,24 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 18:02:52 by victofer          #+#    #+#             */
-/*   Updated: 2023/04/12 18:11:21 by victofer         ###   ########.fr       */
+/*   Updated: 2023/04/18 18:13:22 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	print_test(t_cmd *cmd)
+{
+	int	i;
+
+	i = -1;
+	printf("test cmd -> %s\n", cmd->cmd);
+	printf("test flg -> %s\n", cmd->flags);
+	printf("test params -> %s\n", cmd->params);
+	printf("test nb_out -> %i\n", cmd->nb_outputs);
+	while (++i < cmd->nb_outputs)
+		printf("test out %i -> %i\n", i, cmd->output[i]);
+}
 
 void	leaks(void)
 {
