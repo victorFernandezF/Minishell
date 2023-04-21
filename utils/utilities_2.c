bd38a63451c7c1b80d2fc58bb8d8592a7aae5f2b
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-04-21 12:50:07 +0200

    Refactor: move function to another place

--- utils/utilities_2.c
+++ utils/utilities_2.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 11:51:06 by victofer          #+#    #+#             */
-/*   Updated: 2023/04/21 11:51:41 by victofer         ###   ########.fr       */
+/*   Updated: 2023/04/21 12:31:37 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,20 @@
  * skip_whitespaces
  * ----------------------------
  *	Returns an int with the position of the string
- *	after skipping all initials spaces.
+ *	after skipping all spaces.
+ */
+int	skip_whitespaces(char *str, int i)
+{
+	while (str[i] == ' ')
+		i++;
+	return (i);
+}
+
+/* 
+ * skip_whitespaces
+ * ----------------------------
+ *	Returns an int with the position of the string
+ *	after skipping all characters.
  */
 int	skip_characters(char *str, int i)
 {
