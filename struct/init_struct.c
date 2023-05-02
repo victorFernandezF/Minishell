0365f768c3ac1baa1efbddb6c6de42018baf0609
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-05-02 19:19:37 +0200

    Refactor: initialize inputs to null at the begining

--- struct/init_struct.c
+++ struct/init_struct.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 10:55:59 by victofer          #+#    #+#             */
-/*   Updated: 2023/04/26 11:01:59 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/02 19:19:12 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,9 @@ t_cmd	*init_struct(t_cmd *structure)
 	structure->params = NULL;
 	structure->input = 0;
 	structure->nb_outputs = 0;
+	structure->nb_inputs = 0;
 	structure->output = NULL;
+	structure->input = NULL;
 	structure->index = 1;
 	structure->next = NULL;
 	return (structure);
