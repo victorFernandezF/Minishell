fbe60f3af08baccf51f194a449c18f14750cfac0
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-05-08 18:46:51 +0200

    Feat: add a new line after printing the error.

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:53:25 by victofer          #+#    #+#             */
-/*   Updated: 2023/05/02 19:01:02 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:30:15 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	print_error_file(char *input, char *msg, t_cmd *cmd)
 {
 	(void)cmd;
-	printf("%s: %s", input, msg);
+	printf("%s: %s\n", input, msg);
 }
