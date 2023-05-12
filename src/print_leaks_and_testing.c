102a0fe0ea9acfafda6108c495e70030301db57c
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-05-12 11:00:25 +0200

    test: Activate print and free stuff that wass disabled just for testing.

--- src/print_leaks_and_testing.c
+++ src/print_leaks_and_testing.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 18:02:52 by victofer          #+#    #+#             */
-/*   Updated: 2023/05/12 10:16:47 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/12 10:51:59 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,21 @@ void	print_test(char *str, t_cmd *cmd, int repeat)
 	printf("%s⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽%s\n\n", B, W);
 	printf("%s ● NODE ID ..... ➤%s (%d)%s\n", Y, BG, cmd->index, W);
 	printf("%s ● COMMAND ..... ➤%s %s\n", Y, W, cmd->cmd);
- 	printf("%s ● FLAGS ....... ➤%s %s\n", Y, W, cmd->flags);
-/*	while (cmd->params[++i] != NULL)
+	printf("%s ● FLAGS ....... ➤%s %s\n", Y, W, cmd->flags);
+	while (cmd->params[++i] != NULL)
 		printf("%s ● PARAM [%s%i%s] ... ➤%s %s\n", Y, B, i,
 			Y, W, cmd->params[i]);
+	printf("%s ● NB INPUTS ... ➤%s %i\n", Y, W, cmd->nb_inputs);
+	printf("%s ● LAST INPUT FD ➤%s %i\n", Y, W, cmd->input);
 	printf("%s ● NB OUTPUTS .. ➤%s %i\n", Y, W, cmd->nb_outputs);
 	i = -1;
 	while (++i < cmd->nb_outputs)
 		printf("%s ● OUTPUT FD [%s%i%s] ➤%s %i\n",
 			Y, B, i, Y, W, cmd->output[i]);
-	printf("%s ● NB INPUTS ... ➤%s %i\n", Y, W, cmd->nb_inputs);
-	printf("%s ● LAST INPUT FD ➤%s %i\n", Y, W, cmd->input);
+
 	printf("\n");
 	if (cmd->next != NULL)
-		print_test(str, cmd->next, 1); */
+		print_test(str, cmd->next, 1);
 }
 
 /* 
