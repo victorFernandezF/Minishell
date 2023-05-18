7553d9f5397b8deea6dc4ebb52a928df88c30745
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-05-18 19:23:27 +0200

    test: testing stuffy

--- utils/utilities.c
+++ utils/utilities.c
@@ -6,50 +6,55 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 11:20:40 by victofer          #+#    #+#             */
-/*   Updated: 2023/05/16 13:10:36 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/18 19:22:14 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/* 
- * are_there_char (utils/utilities.c)
- * ----------------------------
- *	Returns 1 if '|' is found in string str.
+/**
+ * @brief Returns 1 if '|' is found in string str.
  *	If not, returns 0.
+ * 
+ * @param c char
+ * @return int 0 or 1. 
  */
 int	is_pipe(char c)
 {
 	return (c == '|');
 }
 
-/* 
- * is_env_var (utils/utilities.c)
- * ----------------------------
- *	Returns 1 if '$' is found in str
+/**
+ * @brief Returns 1 if '$' is found in str
  *	If not, returns 0.
+ * 
+ * @param c char.
+ * @return int 0 or 1.
  */
 int	is_env_var(char c)
 {
 	return (c == '#');
 }
 
-/*
- * is_redirect (utils/utilities.c)
- * ----------------------------
- *	Returns 1 if there is some redirect sign or pipe.
+/**
+ * @brief Returns 1 if there is some redirect sign or pipe.
  *	If not, returns 0.
+ * 
+ * @param c char.
+ * @return int 0 or 1.
  */
 int	is_redirect(char c)
 {
 	return (c == '>' || c == '<');
 }
 
-/* 
- * are_there_char (utils/utilities.c)
- * ----------------------------
- *	Returns 1 if char c is found in string str.
+/**
+ * @brief Returns 1 if char c is found in string str.
  *	If not, returns 0.
+ * 
+ * @param str line.
+ * @param c character to search.
+ * @return int 0 or 1.
  */
 int	are_there_char(char *str, char c)
 {
@@ -62,18 +67,13 @@ int	are_there_char(char *str, char c)
 	return (0);
 }
 
-/* 
- * print_error_file (utils/utilities.c)
- * ----------------------------
- *	This function prints an error when trying to open
- *	a file that does not exist. 
- *
- *	PARAMS:
- *	-> input: The filename. 
- *	-> msg: The message that will be displayed.
- *	-> cmd:	The command structure.
+/**
+ * @brief Prints an error mesage and exit the program.
+ * 
+ * @param name name of the error.
+ * @param msg message to display before exit.
  */
-void	print_error_file(char *input, char *msg)
+void	print_error_file(char *name, char *msg)
 {
 	printf("%s: %s\n", input, msg);
 	exit(-1);
