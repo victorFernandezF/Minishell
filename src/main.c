cea8c1a5742971db2d5d31d65b6c3060ae0bd082
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-05-25 13:45:08 +0200

    Test: trying to remove memory leaks. Yummy!

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:50:03 by victofer          #+#    #+#             */
-/*   Updated: 2023/05/25 13:32:32 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/25 13:44:19 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 void	sighandler(int sig)
 {
 	if (sig == 2)
-	{
 		exit(0);
-	}
 }
 
 int	main(void)
@@ -36,7 +34,8 @@ int	main(void)
 			add_history(read);
 		if (read[0])
 		{
-
+			free_struct(cmd);
+			cmd = init_struct(cmd);
 			cmd = start_parser(cmd, read);
 			if (cmd != NULL)
 				print_test(read, cmd, 0);
