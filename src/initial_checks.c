68fd45c053895d1fc15a84d7ccfb04cb47f17825
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-05-30 12:11:40 +0200

    Fix: fix in the initial checks.

--- src/initial_checks.c
+++ src/initial_checks.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 17:49:00 by victofer          #+#    #+#             */
-/*   Updated: 2023/05/29 19:36:36 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/30 12:10:32 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,11 @@ int	is_pipe_at_end_of_line(char	*str)
 
 /**
  * @brief Checks if there are two or more pipes in a row
+ * 	skipping spaces between them.
  * 
  * @param str 
  * @param i 
- * @return int 
+ * @return 1 if there are more than one pipe in a row. 0 if not.
  */
 int	check_two_pipes_in_a_row(char *str, int i)
 {
@@ -41,21 +42,37 @@ int	check_two_pipes_in_a_row(char *str, int i)
 	{
 		if (str[i] == '|')
 		{
+			i++;
 			i = skip_whitespaces(str, i);
 			if (str[i] == '|')
 				return (1);
 		}
-		return (1);
+		i++;
 	}
 	return (0);
 }
 
+/**
+ * @brief Checks if there are any redirection sign following by
+ * an invalid character.
+ * 
+ * @param str 
+ * @param i 
+ * @return 1 if something is wrong. 0 if not.
+ */
 int	check_bad_redirection_chars(char *str, int i)
 {
 	if (str[i] == '>' && str[i + 1] == '<')
 		return (1);
 	if (str[i] == '<' && str[i + 1] == '>')
 		return (1);
+	if (str[i] == '>' && str[i + 1] != '>')
+	{
+		i++;
+		i = skip_whitespaces(str, i);
+		if (!ft_isalnum(str[i]) && !is_space_minishel(str[i]) && str[i] != '$')
+			return (1);
+	}
 	return (0);
 }
 
@@ -78,11 +95,11 @@ int	check_invalid_characters(char *str)
 	while (str[++i])
 	{
 		if (check_two_pipes_in_a_row(str, i))
-			return (printf("Syntax error.\n"));
+			return (printf("Syntax error 1.\n"));
 		if (check_bad_redirection_chars(str, i))
-			return (printf("Syntax error.\n"));
+			return (printf("Syntax error 2.\n"));
 		if (str[0] == '|' || is_pipe_at_end_of_line(str))
-			return (printf("Syntax error.\n"));
+			return (printf("Syntax error 3.\n"));
 	}
 	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:50:03 by victofer          #+#    #+#             */
-/*   Updated: 2023/05/26 17:19:11 by victofer         ###   ########.fr       */
+/*   Updated: 2023/05/30 11:24:18 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char **argv, char **env)
 {
 	(void)argv;
 	atexit(leaks);
+	printf("%i\n", (int)getpid());
 	if (argc == 1)
 		mini_loop(env);
 	else
