cf609099ddd6e4b61105c7b1998ac5938f2d46fa
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-07-12 12:59:45 +0200

    Refactor: Delete commented stufff

--- get/get_flags.c
+++ get/get_flags.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 11:09:26 by victofer          #+#    #+#             */
-/*   Updated: 2023/07/12 12:55:12 by victofer         ###   ########.fr       */
+/*   Updated: 2023/07/12 12:59:21 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,19 +60,3 @@ char	*get_flags(char *exp, char *cmd_line, t_cmd *cmd)
 	free_maximun_of_four_str(no_cmd, copy, exp, NULL);
 	return (rest);
 }
-
-/* 	
-	start = 0;
-		if (cmd_line[0] != '-')
-			return (cmd_line);
-		end = get_position_of_last_char_found(cmd_line, '-');
-		end = skip_characters(cmd_line, end);
-		flags = ft_substr(cmd_line, start, end);
-		if (!flags)
-		{
-			cmd->flags = NULL;
-			return (cmd_line);
-		}
-		cmd->flags = flags;
-		rest = ft_substr(cmd_line, end, ft_strlen(cmd_line));
-		free(cmd_line); */
