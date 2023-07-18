cf1cab73e7583207b0296a4ec4b0402f8a08e3dc
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-07-18 14:27:18 +0200

    Fix: print $1PID

--- utils/utils_checks_1.c
+++ utils/utils_checks_1.c
@@ -72,7 +72,7 @@ int	env_var_detector(char *str)
 	i = -1;
 	while (str[++i])
 		if (is_env_var(str[i])
-			&& (ft_isalpha(str[i + 1]) || str[i + 1] == '?' || str[i + 1] == '_'))
+			&& (ft_isalnum(str[i + 1]) || str[i + 1] == '?' || str[i + 1] == '_'))
 			return (1);
 	return (0);
 }
